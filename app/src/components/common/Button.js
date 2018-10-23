d08a9c75c1750e3b3f53aa175169f16f6a42bb92
Denis Khvostov <talionarwork@gmail.com>
2018-10-23 21:33:35 +0300

    feat(common): add link styled Button

--- app/src/components/common/Button.js
+++ app/src/components/common/Button.js
@@ -16,11 +16,11 @@ import styled from 'styled-components';
 export const Button = styled.button`
   word-break: break-word;
   font-size: 1em;
-  padding: 0.45em 1.3em;
+  padding: ${({ link }) => (link ? '0' : '0.45em 1.3em')};
   border-radius: 0.2em;
   color: ${({ primary }) => (primary ? 'var(--colors-text-accent)' : 'var(--colors-bg-accent)')};
   background-color: ${({ primary }) => (primary ? 'var(--colors-bg-accent)' : null)};
-  border: 0.1em solid var(--colors-bg-accent);
+  border: ${({ link }) => (link ? 'none' : '0.1em solid var(--colors-bg-accent)' )};
   font-weight: 600;
   text-align: center;
 `;

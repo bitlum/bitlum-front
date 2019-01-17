8a6e02dabda9449003d4329d48458c9aa1a0d65e
Denis Khvostov <talionarwork@gmail.com>
2019-01-17 11:03:32 +0300

    feat: add permissions request on lvh.me for development

--- app/src/scenes/Login/view.js
+++ app/src/scenes/Login/view.js
@@ -9,16 +9,31 @@
 
 import React from 'react';
 
-import { Root, LoginForm, Logo, A, Span } from './styles';
+import { Root, LoginForm, Logo, A, Span, Button } from './styles';
 
 // -----------------------------------------------------------------------------
 // Code
 // -----------------------------------------------------------------------------
 
-const Login = ({history}) => (
+const Login = ({ history }) => (
   <Root>
     <Logo />
     <LoginForm />
+    {process.env.NODE_ENV === 'development' ? (
+      <Button
+        primary
+        onClick={() => {
+          window.chrome.permissions.request(
+            {
+              origins: ['http://lvh.me/*'],
+            },
+            () => {},
+          );
+        }}
+      >
+        Enable access to lvh.me
+      </Button>
+    ) : null}
     <A
       onClick={() => {
         history.push(`/signup`);
